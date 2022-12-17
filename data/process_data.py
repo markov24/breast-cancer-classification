f46a4a3a41127a0ba3df5b1a762c2c61dc6f1c0e
Marko Veljanovski <markoveljanovski2025@u.northwestern.edu>
2022-12-17 17:34:10 -0600

    add optional shuffle parameter

--- data/process_data.py
+++ data/process_data.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 # Create training data from raw numpy data
-def generate_data():
+def generate_data(shuffle=True):
     # Convert raw data into a NumPy array
     with open('tumor_data.txt', 'r') as f:
         data = np.array([[e for e in line.split(',')] for line in f])
 
     # Schuffle data so train/test split is random
-    np.random.shuffle(data)
+    if shuffle:
+        np.random.shuffle(data)
     return data
