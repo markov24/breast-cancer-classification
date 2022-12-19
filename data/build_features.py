aa76cc0e7d19f2288a395ed20ce337389dc368c5
Marko Veljanovski <markoveljanovski2025@u.northwestern.edu>
2022-12-19 09:03:30 +0100

    Change data to distinct X, y matrices

--- data/build_features.py
+++ data/build_features.py
@@ -1,5 +1,4 @@
 import numpy as np
-from data import process_data as pd
 
 # Build model with x^2 features
 def square_model(X):
@@ -32,12 +31,12 @@ def normalize(X):
 # Generate training data with two optional parameters:
 #   model: 1-standard, 2-squared, 3-cubic
 #   train: train/test split, default of 90% training data
-def training_data(features, train=0.9, model=1):
-    data = pd.generate_data()
-    cutoff = round(train*data.shape[0])
+def training_data(X, y, features, train=0.9, model=1):
+    # Select cutoff
+    cutoff = round(train*X.shape[0])
 
-    X_train = np.array(data[:cutoff, 2:(2+features)], dtype=float)
-    X_test = np.array(data[cutoff:, 2:(2+features)], dtype=float)
+    X_train = np.array(X[:cutoff, :features], dtype=float)
+    X_test = np.array(X[cutoff:, :features], dtype=float)
 
     # Select chosen model and normalize
     if model==2:
@@ -46,14 +45,6 @@ def training_data(features, train=0.9, model=1):
         X_train = cubic_model(X_train)
     X_train = normalize(X_train)
 
-    # Format y into 1 - malignant, 0 - benign
-    y = np.array(data[:, 1])
-    for i in range(len(y)):
-        if y[i] == 'M':
-            y[i] = 1
-        else:
-            y[i] = 0
-    y = np.array(y, dtype=float)
     y_train = np.array(y[:cutoff])
     y_test = np.array(y[cutoff:])
 
--- data/process_data.py
+++ data/process_data.py
@@ -1,12 +1,17 @@
 import numpy as np
 
 # Create training data from raw numpy data
-def generate_data(shuffle=True):
+def generate_data():
     # Convert raw data into a NumPy array
     with open('data/tumor_data.txt', 'r') as f:
         data = np.array([[e for e in line.split(',')] for line in f])
 
-    # Schuffle data so train/test split is random
-    if shuffle:
-        np.random.shuffle(data)
-    return data
+    X = np.array(data[:, 2:12], dtype=float)
+    y = np.array(data[:, 1])
+    for i in range(len(y)):
+        if y[i] == 'M':
+            y[i] = 1
+        else:
+            y[i] = 0
+    y = np.array(y, dtype=float)
+    return X, y
