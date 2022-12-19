64a1df8abf4f09c5985c2c3c7d44e4cfd59745b5
Marko Veljanovski <markoveljanovski2025@u.northwestern.edu>
2022-12-19 06:29:08 +0100

    import data fix

--- data/process_data.py
+++ data/process_data.py
@@ -3,7 +3,7 @@ import numpy as np
 # Create training data from raw numpy data
 def generate_data(shuffle=True):
     # Convert raw data into a NumPy array
-    with open('tumor_data.txt', 'r') as f:
+    with open('data/tumor_data.txt', 'r') as f:
         data = np.array([[e for e in line.split(',')] for line in f])
 
     # Schuffle data so train/test split is random
