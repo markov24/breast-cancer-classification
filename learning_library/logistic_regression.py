e35b6f57fa858fa047f80069d74187ab03572407
Marko Veljanovski <markoveljanovski2025@u.northwestern.edu>
2022-12-19 10:54:21 +0100

    alter iteration of matplotlib plot

--- learning_library/logistic_regression.py
+++ learning_library/logistic_regression.py
@@ -43,27 +43,28 @@ def gradient_descent(iter, X_train, y_train, a, l):
     dw_vec = np.zeros(n)
     # Print out status 10 times
     for i in range(iter):
-        if i%(iter//10) == 0:
-            iter_vec.append(i)
-            cost_vec.append(compute_cost(X_train, y_train, w_vec, b, l))
-            print(f"Iteration: {i}")
+        iter_vec.append(i)
+        cost = compute_cost(X_train, y_train, w_vec, b, l)
+        cost_vec.append(cost)
         dw_vec, db = compute_gradient(X_train, y_train, w_vec, b, l)
         w_vec = w_vec - a*dw_vec
         b = b - a*db
+        if i%(iter//10) == 0:
+            print(f"Iteration: {i}  Cost: {cost}")
     iter_vec, cost_vec = iter_vec[1:], cost_vec[1:]
     plt.plot(iter_vec, cost_vec)
     plt.show()
     return w_vec, b
 
-def predict(X_train, y_train, w_vec, b):
+def predict(X_test, y_test, w_vec, b):
     print(f"Parameters of model are: {w_vec} and {b}")
-    m, n = X_train.shape
+    m, n = X_test.shape
     correct = 0
     for i in range(m):
-        z = np.dot(X_train[i], w_vec) + b
+        z = np.dot(X_test[i], w_vec) + b
         prediction = sigmoid(z)
-        if prediction >= 0.5 and y_train[i] == 1:
+        if prediction >= 0.5 and y_test[i] == 1:
             correct += 1
-        elif prediction < 0.5 and y_train[i] == 0:
+        elif prediction < 0.5 and y_test[i] == 0:
             correct += 1
     print(f"Model accuracy is {round(100*correct/m, 3)}%")
